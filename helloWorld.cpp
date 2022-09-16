4216d7f22cdf4bcf4c10be2d2fe7d9a04f92e3e0
noah daher <noah.daher@mths.ca>
2022-09-16 15:20:25 +0000

    Made changes

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,4 +1,8 @@
-// Copyright 2022 Noah Daher
+// Copyright (c) 2022 Noah Daher All rights reserved
+//
+// Created by: Noah Daher
+// Created on: Sept 2022
+// This is the "Hello, World!" program, with proper style
 
 #include <iostream>
 
